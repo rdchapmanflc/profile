1f4352d8ce538b0e29a32286263901af02afc22f
Rory Chapman <rdchapman@fortlewis.edu>
2018-08-03 15:29:27 -0600

    Added include for macospcclient

--- manifests/lab_baseline.pp
+++ manifests/lab_baseline.pp
@@ -10,11 +10,10 @@
 class profile::lab_baseline {
   include munki
   include macos_firefox_config::stdlabs_managed_prefs
-  #include macos_localusers::admin_users
   include macos_mgmt_profile
   include macos_mgmt_profile::baseline_mgmt_profile
   include macos_mgmt_profile::google_chrome_preferences
   include macos_mgmt_profile::google_software_updates
-  
+  include macospcclient
 }
 
